0989dccf2d5cff863739a6d16091d344380fbc39
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-11-24 16:55:48 +0100

    Moving css import to the end of import stack.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import './index.css';
 import AppComponent from './ui/AppComponent';
 import OrdersReducer from './redux/reducers/OrdersReducer';
 import UiReducer from './redux/reducers/UiReducer';
 import UserReducer from './redux/reducers/UserReducer';
+import './index.css';
 
 const rootReducer = combineReducers({
   orders: OrdersReducer,
