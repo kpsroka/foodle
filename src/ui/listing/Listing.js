06d47c0325af7c405edab7b72c0893e1c3b8383e
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-11-27 15:02:57 +0100

    Adding ListingHeader and Order components in Listing component.

--- src/ui/listing/Listing.js
+++ src/ui/listing/Listing.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ListingHeader from './ListingHeader';
+import * as React from 'react';
+import ListingHeaderComponent from './ListingHeaderComponent';
+import OrderComponent from './OrderComponent';
+import OrderDetailsComponent from './OrderDetailsComponent';
+import type { Order } from '../../redux/state/State';
 
-export default function Listing() {
-  return (
-    <div>
-      <ListingHeader/>
-    </div>
-  );
+export type ListingProps = {|
+  orders: Array<Order>,
+  expandedOrderIndex: number
+|};
+
+export default class Listing extends React.PureComponent<ListingProps> {
+  renderDetailsComponent(index, order) {
+    return this.props.expandedOrderIndex === index ? <OrderDetailsComponent order={order} index={index} /> : null;
+  };
+
+  render() {
+    return (
+        <div>
+          <ListingHeaderComponent />
+          {this.props.orders.map((order, index) => (
+              <OrderComponent
+                  key={index}
+                  index={index}
+                  order={order}
+                  expanded={this.props.expandedOrderIndex === index}>
+                {this.renderDetailsComponent(this.props.expandedOrderIndex === index, order)}
+              </OrderComponent>
+          ))}
+        </div>
+    );
+  }
 }
--- src/ui/listing/ListingComponent.js
+++ src/ui/listing/ListingComponent.js
@@ -1,7 +1,22 @@
+// @flow
+
 import { connect } from 'react-redux';
 import Listing from './Listing';
+import type { State } from '../../redux/state/State';
+import type { ListingProps } from './Listing';
 
-const mapStateToProps = () => ({});
+function mapStateToProps(state:State):ListingProps {
+  const listingMode = state.ui.listingMode;
+  const orderData = state.orders;
+  if (listingMode && orderData) {
+    return {
+      orders: listingMode.list === 'ACTIVE' ? orderData.activeOrders : orderData.historicOrders,
+      expandedOrderIndex: listingMode.expandedOrderIndex
+    };
+  }
+
+  throw new Error('Illegal state');
+}
 
 const ListingComponent = connect(mapStateToProps)(Listing);
 
