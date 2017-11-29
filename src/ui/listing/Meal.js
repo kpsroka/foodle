714f95facadd60547a860845d9c4d3b22d4793dc
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-11-29 12:08:26 +0100

    Adding an overlay to Meal to prevent interaction with non-editable entries.

--- src/ui/listing/Meal.js
+++ src/ui/listing/Meal.js
@@ -22,6 +22,7 @@ export default function Meal(props:MealProps) {
         <div className="MealControl fa fa-pencil-square-o" aria-hidden="true" />
         <div className="MealControl fa fa-trash" aria-hidden="true" />
       </div>
+      <div className="DisableControlOverlay" />
     </div>
   );
 }
