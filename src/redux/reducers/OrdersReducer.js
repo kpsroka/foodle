88544d59979a4fee06bcf2d365a2179c0eb1d392
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-11-29 14:27:46 +0100

    Adding handling of DeleteMeal action in OrdersReducer. Also adding dot-prop-immutable to help with state manipulation.

--- src/redux/reducers/OrdersReducer.js
+++ src/redux/reducers/OrdersReducer.js
@@ -1,6 +1,10 @@
-import type { Meal, OrderData } from '../state/State';
+// @flow
+
+import * as dotProp from 'dot-prop-immutable';
+import type { ListingModeList, Meal, OrderData } from '../state/State';
 import type { Action, AddMealAction } from '../actions/Actions';
 import { parsePrice } from '../../PriceFormatter';
+
 
 function createMeal(action:AddMealAction):Meal {
   const payload = action.payload;
@@ -11,22 +15,41 @@ function createMeal(action:AddMealAction):Meal {
   };
 }
 
-export default function OrdersReducer(orderData:?OrderData, action:Action):OrderData {
+function listStringToKey(listString:ListingModeList):string {
+  switch (listString) {
+    case 'ACTIVE': return 'activeOrders';
+    case 'HISTORY': return 'historicOrders';
+    default: throw new Error(`Unrecognized list ${listString}`);
+  }
+}
+
+export default function OrdersReducer(orderData:?OrderData, action:Action):?OrderData {
   if (orderData === undefined) {
     return null;
   }
 
   switch (action.type) {
-    case 'ADD_MEAL':
-      const order = { ...orderData.activeOrders[action.payload.orderIndex] };
+    case 'ADD_MEAL': {
+      if (orderData === null) { throw new Error('Illegal state'); }
+      const order = {...orderData.activeOrders[action.payload.orderIndex]};
       order.meals = order.meals.slice();
       order.meals.push(createMeal(action));
       const activeOrders = orderData.activeOrders.slice();
       activeOrders[action.payload.orderIndex] = order;
-      return { ...orderData, activeOrders };
-    case '_SET_ORDER_DATA':
+      return {...orderData, activeOrders};
+    }
+    case 'DELETE_MEAL': {
+      if (orderData === null) { throw new Error('Illegal state'); }
+      const listKey = listStringToKey(action.payload.list);
+      return dotProp.delete(
+          orderData,
+          `${listKey}.${action.payload.orderIndex}.meals.${action.payload.mealIndex}`);
+    }
+    case '_SET_ORDER_DATA': {
       return action.payload.orders;
-    default:
+    }
+    default: {
       return orderData;
+    }
   }
 }
--- src/redux/reducers/OrdersReducer.test.js
+++ src/redux/reducers/OrdersReducer.test.js
@@ -1,26 +1,31 @@
 import OrdersReducer from './OrdersReducer';
 import SetOrderData from '../actions/SetOrderData';
 import AddMeal from '../actions/AddMeal';
+import DeleteMeal from '../actions/DeleteMeal';
 
 describe('OrdersReducer', () => {
   const defaultOrders = {
     activeOrders: [
-      { state: 'OPEN', owner: "Foo", restaurant: "Bar", meals: [] },
+      { state: 'OPEN', owner: 'Foo', restaurant: 'Bar', meals: [] },
       {
         state: 'FINALIZED',
-        owner: "Foo",
-        restaurant: "Baz",
-        meals: [{ orderer: "Joe", name: "Broccoli", priceE2: 123 }]
+        owner: 'Foo',
+        restaurant: 'Baz',
+        meals: [
+          { orderer: 'Joe', name: 'Broccoli', priceE2: 123 },
+          { orderer: 'Juan', name: 'Pizza', priceE2: 1099 },
+          { orderer: 'Jan', name: 'Onion (large)', priceE2: 49 },
+        ]
       }
     ],
     historicOrders: [
       {
         state: 'DELIVERED',
-        owner: "Jane",
-        restaurant: "Baz",
+        owner: 'Jane',
+        restaurant: 'Baz',
         meals: [
-          { orderer: "Joe", name: "Cauliflower", priceE2: 124 },
-          { orderer: "Jane", name: "Romanesco", priceE2: 404 }
+          { orderer: 'Joe', name: 'Cauliflower', priceE2: 124 },
+          { orderer: 'Jane', name: 'Romanesco', priceE2: 404 }
         ]
       }
     ]
@@ -29,13 +34,13 @@ describe('OrdersReducer', () => {
   describe('on SetOrderData action', () => {
     test('replaces order data with action payload', () => {
       const newOrders = {
-        activeOrders: [{ state: 'OPEN', owner: "Foo", restaurant: "Bar", meals: [] }],
+        activeOrders: [{ state: 'OPEN', owner: 'Foo', restaurant: 'Bar', meals: [] }],
         historicOrders: [
           {
             state: 'DELIVERED',
-            owner: "Foo",
-            restaurant: "Baz",
-            meals: [{ orderer: "Joe", name: "Broccoli", priceE2: 123 }]
+            owner: 'Foo',
+            restaurant: 'Baz',
+            meals: [{ orderer: 'Joe', name: 'Broccoli', priceE2: 123 }]
           }
         ]
       };
@@ -63,7 +68,27 @@ describe('OrdersReducer', () => {
           .toHaveLength(defaultOrders.activeOrders[orderIndex].meals.length + 1);
       expect(newOrders.activeOrders[orderIndex].meals[newOrders.activeOrders[orderIndex].meals.length - 1])
           .toMatchObject({ name: mealName, orderer, priceE2: 2340 });
+    });
+  });
 
+  describe('on DeleteMeal action', () => {
+    test('deletes specified meal', () => {
+      const list = 'ACTIVE';
+      const orderIndex = 1;
+      const mealIndex = 1;
+
+      const action = DeleteMeal(list, orderIndex, mealIndex);
+      const newOrders = OrdersReducer(defaultOrders, action);
+
+      expect(newOrders).not.toBe(defaultOrders);
+      expect(newOrders.historicOrders).toBe(defaultOrders.historicOrders);
+      expect(newOrders.activeOrders).not.toBe(defaultOrders.activeOrders);
+
+      const newOrdersList = newOrders.activeOrders;
+      const oldOrdersList = defaultOrders.activeOrders;
+      expect(newOrdersList).toHaveLength(oldOrdersList);
+      expect(newOrdersList[orderIndex].meals).toHaveLength(oldOrdersList[orderIndex].meals.length - 1);
+      expect(newOrdersList[orderIndex].meals).not.toContain(oldOrdersList[orderIndex].meals[mealIndex]);
     });
   });
 });
